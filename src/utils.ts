9295839ab248bb9ea96da9dff2e057a4154501f0
Kharya1337 <ikharlamov21@gmail.com>
2025-02-10 18:49:24 +0200

    fix(utils): enhance error handling in installGoodKey function

--- src/utils.ts
+++ src/utils.ts
@@ -59,7 +59,8 @@ export async function installGoodKey(distDir: string, systemDir: string) {
   } catch (error) {
     if (error instanceof Error) {
       const message = 'stdout' in error && error.stdout ? error.stdout.toString() : error.message;
-      throw new Error(`Installation of GoodKey failed: ${message}`);
+      const stack = 'stderr' in error && error.stderr ? error.stderr.toString() : error.stack;
+      throw new Error(`Installation of GoodKey failed: ${message}, ${stack}`);
     }
     throw error;
   }
